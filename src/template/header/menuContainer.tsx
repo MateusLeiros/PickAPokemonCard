43639c64f66e1b1ca34dc158d4fb242d6af6253f
Mateus Leiros <mateusleiros@gmail.com>
2024-12-12 08:51:56 -0300

    criado componente menuContainer, onde podem ser incluidos botões para o header

--- src/template/header/menuContainer.tsx
+++ src/template/header/menuContainer.tsx
@@ -3,11 +3,13 @@ import styles from './MenuContainer.module.css';
 
 const MenuContainer = () => {
     return  <div className={styles.header}>
-                <button>NFT Marketplace</button>
-                <button>Marketplace</button>
-                <button>Rankings</button>
-                <button>Connect a wallet</button>
-                <button>Sign Up</button>
+                <div>NFT Marketplace</div>
+                <div>
+                    <button>Marketplace</button>
+                    <button>Rankings</button>
+                    <button>Connect a wallet</button>
+                    <button>Sign Up</button>
+                </div>
             </div>;
 }
 
