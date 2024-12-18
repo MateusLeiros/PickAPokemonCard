75d8154616adfedc50467054bf7a6b13396f0e3b
Mateus Leiros <mateusleiros@gmail.com>
2024-12-17 22:26:52 -0300

    Alterações no estilo do botão, agora com estilo para botão enabled e disabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <Button />
+      <Button label='Botão' disabled onClick={() => {console.log('botao');}} />
+      <Button label='blablabla whiskas sache' onClick={() => {console.log('teste')}} />
       </div>
     </>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,35 @@ import React from 'react';
 import styles from './Button.module.css';
 import SignInIcon from '../assets/signIn.svg';
 
-const Button = () => {
+const Button = (props:buttonProps) => {
+    
+    const [loading, setLoading] = React.useState(false);
+    function handleClick() {
+        setLoading(!loading);
+        console.log(loading);
+    }
+
+
     return (
-            <button className={styles.button}>
-                <div className={styles.textoBotao}>
-                    <img src={SignInIcon} /> Sign in                    
+            <button disabled={props.disabled} onClick={handleClick} className={styles.button}>
+                <div className={styles.textButton}>
+                    <img src={SignInIcon} /> <span>{props.label}</span>                    
                 </div>
             </button>
     )
 }
 
+type buttonProps = {
+    disabled?: boolean,
+    label: string,
+    className?: string,
+    onClick: () => void,
+    // isLoading?: boolean,
+    image?: string,
+} /*& typeof defaultButtonProps*/;
+
+// const defaultButtonProps = {
+//     classname: 'styles.button',
+// }
 
 export default Button;
