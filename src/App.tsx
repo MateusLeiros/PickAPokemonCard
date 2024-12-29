20a78919029bfdd403f802d2fab5c69502ff4af8
Daniel <contato@danielporto.com.br>
2024-12-29 19:36:47 -0300

    refactor: remove unused test components and styles; enhance Button component with loading state and icon support

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import React from "react";
 import "./App.css";
+import SignIn from "./assets/signIn.svg?react";
 import Button from "./components/Button.tsx";
-import Test from "./test.tsx";
 
 function App() {
-  /* Eu fiz o loading ser aqui na app da forma que eu comentei aqui debaixo:
-  
   const [loading, setLoading] = React.useState(false);
-  
+
   function handleClick() {
     setLoading(true);
-    setTimeout(() => setLoading(false), [2000]);
+    console.log("Button clicked");
+    setTimeout(() => {
+      console.log("Button loading finished");
+      setLoading(false);
+    }, 2000);
   }
-
-  Problema: Quando clicava em um botão, todos entravam no seu estado de loading 
-  juntos e mudavam pros seus respectivos textos setados no botão la em baixo.
-
-  Solução: Função de loading movida pra dentro do componente botão.
-  
-  */
 
   return (
     <div>
-      <Button label='Botão' disabled />
-      <Button label='blablabla whiskas sache' />
-      <Test />
+      <Button
+        label={"Botão"}
+        onClick={handleClick}
+        isLoading={loading}
+        icon={<SignIn />}
+      />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,44 +1,36 @@
-import React from 'react';
-import styles from './Button.module.css';
-import SignIn from "../assets/signIn.svg?react";
+import styles from "./Button.module.css";
 
-const Button = (props:buttonProps) => {
-    const [loading, setLoading] = React.useState(false);
-  
-    const getPokemonCard = async () => {
-        // console.log("começou o fetch")
-        const response = await fetch(
-            "https://api.tcgdex.net/v2/en/cards/swsh3-136"
-        );
-        const json = await response.json();
-        console.log(json);
-        // console.log("terminou o fetch");
-        setLoading(false);
-    };
-    
-    React.useEffect(() => {
-    // console.log("entrou no useEffect");
-    getPokemonCard();
-    // console.log("saiu do useEffect");
-    }, [loading]);
-    
-    return (
-            <button disabled={props.disabled} onClick={() => {setLoading(true)}} className={styles.button + ' ' + props.className}>
-                <div className={styles.textButton}>
-                    <SignIn className={styles.svgIcon} /><span>{props.isLoading || loading? props.loadingText || "Loading..." : props.label}</span> 
-                </div>
-            </button>
-    )
+export default function Button2({
+  className,
+  label,
+  isLoading,
+  disabled,
+  loadingText = "Loading...",
+  onClick,
+  icon,
+}: buttonProps) {
+  const buttonClasses = [className, styles.button];
+  const labelText = isLoading ? loadingText : label;
+
+  return (
+    <button
+      className={buttonClasses.join(" ")}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <div className={styles.label}>
+        <div className={styles.icon}>{icon}</div>
+        <span>{labelText}</span>
+      </div>
+    </button>
+  );
 }
-
 type buttonProps = {
-    disabled?: boolean,
-    label: string,
-    className?: string,
-    onClick?: () => void,
-    image?: string,
-    isLoading?: boolean,
-    loadingText?: string,
-}
-
-export default Button;
+  disabled?: boolean;
+  label: string;
+  className?: string;
+  onClick?: () => void;
+  icon?: React.ReactElement;
+  isLoading?: boolean;
+  loadingText?: string;
+};
