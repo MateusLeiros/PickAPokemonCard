257554b78dbfd6d92fbea7a905c4c78925db5cf5
Mateus Leiros <mateusleiros@gmail.com>
2025-01-08 10:57:26 -0300

    Header do site criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
-import Menu from "./template/header/Menu.tsx";
+import Header from "./template/header/Header.tsx";
 
 function App() {
 
-  const links = [{name:'See Pokémons', url:'#'}, {name:'Create a Deck', url:'#'}, {name:'Contact', url:'#'}, {name:'Mini-game (soon)', url:'#'}];
-
   return (
     <div>
-      <Menu links={links}/>
+      <Header/>
     </div>
   );
 }
---
+++ src/template/header/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Header.module.css";
+import Menu from "./Menu.tsx"
+import PokemonIcon from "../../assets/pbIcon.svg?react";
+
+
+
+export default function Header() {
+    const links = [{name:'See Pokémons', url:'#'}, {name:'Create a Deck', url:'#'}, {name:'Contact', url:'#'}, {name:'Mini-game (soon)', url:'#'}];
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <PokemonIcon className={styles.pbIcon}/><span className={styles.logoText}>Pick a Pokemon Card</span>
+            </div>
+            <Menu links={links}/>
+        </div>
+    );
+}
