46bc03b716c987094e33fd02af4c385c7c3cb29b
Mateus Leiros <mateusleiros@gmail.com>
2025-01-09 11:17:16 -0300

    Correção do menu, pequenas alterações no header

--- src/template/header/Header.tsx
+++ src/template/header/Header.tsx
@@ -2,17 +2,21 @@ import styles from "./Header.module.css";
 import Menu from "./Menu.tsx";
 import PokemonIcon from "../../assets/pbIcon.svg?react";
 
-
-
 export default function Header() {
-    const links = [{name:'See Pokémons', url:'#'}, {name:'Create a Deck', url:'#'}, {name:'Contact', url:'#'}, {name:'Mini-game (soon)', url:'#'}];
+  const links = [
+    { name: "See Pokémons", url: "#" },
+    { name: "Create a Deck", url: "#" },
+    { name: "Contact", url: "#" },
+    { name: "Mini-game (soon)", url: "#" },
+  ];
 
-    return (
-        <div className={styles.header}>
-            <div className={styles.logo}>
-                <PokemonIcon className={styles.pbIcon}/><span className={styles.logoText}>Pick a Pokemon Card</span>
-            </div>
-            <Menu links={links}/>
-        </div>
-    );
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <PokemonIcon className={styles.pbIcon} />
+        <span className={styles.logoText}>Pick a Pokemon Card</span>
+      </div>
+      <Menu links={links} />
+    </div>
+  );
 }
