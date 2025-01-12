b229c2481fc9856f4a6befad0dc896d47feae33b
Mateus Leiros <mateusleiros@gmail.com>
2025-01-12 18:51:39 -0300

    tailwind instalado

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/*.{js,ts,jsx,tsx}",
+    "./src/components/*.{js,ts,jsx,tsx}",
+    "./src/template/*.{js,ts,jsx,tsx}",
+    "./src/template/header/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
