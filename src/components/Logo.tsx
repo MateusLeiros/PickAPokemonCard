cd1c75b92242b80ea3c3dafde72692e6f91c5422
Mateus Leiros <mateusleiros@gmail.com>
2025-01-17 15:16:28 -0300

    pequeno ajuste no componente logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,12 @@
 import PokemonIcon from "../assets/pbIcon.svg?react";
 
 export default function Logo() {
-
   return (
-    <div>
-      <div className="flex items-center gap-x-2.5 cursor-pointer">
-        <PokemonIcon className="w-8 h-8" />
-        <span className="font-WorkSansSemiBold text-white text-xl">Pick a Pokemon Card</span>
-      </div>
+    <div className="flex items-center gap-x-2.5 cursor-pointer">
+      <PokemonIcon className="w-8 h-8" />
+      <span className="font-WorkSansSemiBold text-white text-xl">
+        Pick a Pokemon Card
+      </span>
     </div>
   );
 }
