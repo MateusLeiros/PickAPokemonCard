99bd3db59f2cc5afcb172d5f4d34e55a0ef6bb0b
Mateus Leiros <mateusleiros@gmail.com>
2025-01-27 20:12:52 -0300

    arquivo GetCard, função fetch e chamada de função fetch na home page

---
+++ src/api/GetCard.tsx
@@ -0,0 +1,21 @@
+export function getCardByID(pkmId: number) {
+  const api = "https://api.tcgdex.net/v2/en/cards/swsh3-" + pkmId;
+  const result = fetch(api)
+    .then((response) => response.json())
+    .then((card) => {
+      console.log(card.name);
+    });
+
+  return result;
+}
+
+export async function getCardByID2(pkmId: number) {
+  try {
+    const api = "https://api.tcgdex.net/v2/en/cards/swsh3-" + pkmId;
+    const response = await fetch(api);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,35 @@
+import React from "react";
+import { getCardByID, getCardByID2 } from "../api/GetCard.tsx";
+
 export default function Home() {
+  const [cardData, setCardData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  const pkmId = Math.floor((Math.random() * (201))+1);
+
+  React.useEffect(() => {
+    const fetchCardData = async () => {
+      try {
+        const data = await getCardByID2(pkmId);
+        setCardData(data);
+      } catch (err) {
+        setError('Erro ao carregar os dados.');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCardData();
+  }, []);
+  console.log(cardData);
+  
   return (
     <div>
       <h1>Essa é a Home</h1>
+      <label>{cardData===null? '' : cardData.name}</label>
     </div>
   );
+  
+
 }
