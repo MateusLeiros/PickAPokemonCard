bcf8fd368b997a0b31435621c5c121dac670f0c2
Mateus Leiros <mateusleiros@gmail.com>
2025-01-29 20:22:33 -0300

    iniciada estilização da carta apresentada na home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import LoadingPBIcon from "../components/LoadingPBIcon.tsx";
-import {
-  getCardByID,
-} from "../api/GetCard.tsx";
+import { getCardByID } from "../api/GetCard.tsx";
+import pkmBack from "../assets/pkmBack.jpg";
 
 export default function Home() {
   const [cardData, setCardData] = React.useState(null);
@@ -19,28 +18,31 @@ export default function Home() {
     } catch (err) {
       setError(err);
     }
-  }
+  };
 
   React.useEffect(() => {
     fetchCardData();
   }, []);
-  
+
   let pkmImg = "";
   if (cardData) {
-    pkmImg = cardData.image + "/high.png";
+    pkmImg = cardData.image + "/low.png";
   }
 
   return (
     <div>
       <h1>Essa é a Home</h1>
-      <LoadingPBIcon isLoading={loading}></LoadingPBIcon>
-      <label className="text-4xl">
-        {cardData === null ? "" : cardData.name}
-      </label>
-      <img
-        src={pkmImg}
-        className="border-solid border-white border-4 rounded-s-3xl"
-      ></img>
+      <div className="flex flex-col items-center h-[432px] w-[90vw] border-4 border-blue-500 rounded-3xl">
+        <LoadingPBIcon isLoading={loading}></LoadingPBIcon>
+        <label className="text-4xl">
+          {cardData === null ? "" : cardData.name}
+        </label>
+        <img
+          src={loading ? pkmBack : pkmImg}
+          className="max-h-[350px] border-4 border-blue-500 rounded-3xl brightness-50 transition duration-700 hover:scale-[1.2] hover:brightness-100"
+        ></img>
+      </div>
     </div>
   );
 }
+// brightness-50 hover:scale-[1.2] hover:brightness-100
